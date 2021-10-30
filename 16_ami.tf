9dc28c82673b10eb6bdb1e075472c9075c91c1eb
ksd7248 <ksd7248@naver.com>
2021-10-30 23:07:49 +0900

    ami_step16-2

--- 16_ami.tf
+++ 16_ami.tf
@@ -1,4 +1,7 @@
 resource "aws_ami_from_instance" "sdkim_ami" {
   name                    = "sdkim-ami"
   source_instance_id      = aws_instance.sdkim_weba.id
+  depends_on = [
+    aws_instance.sdkim_weba
+  ]
 }
