9f8bb1aaee786c116b1cf9bb616dc8aa6cbe917d
ksd7248 <ksd7248@naver.com>
2021-10-30 23:07:17 +0900

    web-1_step11-2

--- 11_web.tf
+++ 11_web.tf
@@ -3,7 +3,7 @@ data "aws_ami" "amzn" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
@@ -15,20 +15,19 @@ data "aws_ami" "amzn" {
 }
 
 resource "aws_instance" "sdkim_weba" {
-  ami                    = data.aws_ami.amzn.id
+  ami                    = "ami-0e4a9ad2eb120e054"                             #data.aws_ami.amzn.id
   instance_type          = "t2.micro"
   key_name               = "sdkim-key"
   vpc_security_group_ids = [aws_security_group.sdkim_websg.id]
   availability_zone      = "ap-northeast-2a"
   private_ip             = "10.0.0.11"
   subnet_id              = aws_subnet.sdkim_puba.id
-  user_data              =  file("install_seoul.sh") 
+  user_data              = file("./install_seoul.sh") 
 
   tags = {
     Name = "sdkim-weba"
   }
 }
-
 resource "aws_eip" "sdkim_web_eip" {
   vpc = true
   instance                    = aws_instance.sdkim_weba.id
