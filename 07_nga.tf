66938040aef6b6f642153afbda5996d700912f4c
sdkim <ksd7248@gmail.com>
2021-11-01 08:56:54 +0900

    nag_step7-1

--- 07_nga.tf
+++ 07_nga.tf
@@ -10,7 +10,7 @@ resource "aws_eip" "lb_ip_c" {
 
 resource "aws_nat_gateway" "sdkim_nga_a" {
   allocation_id = aws_eip.lb_ip_a.id
-  subnet_id     = aws_subnet.sdkim_pria.id
+  subnet_id     = aws_subnet.sdkim_puba.id
   tags = {
     Name = "sdkim-nga-a"
   }
@@ -18,7 +18,7 @@ resource "aws_nat_gateway" "sdkim_nga_a" {
 
 resource "aws_nat_gateway" "sdkim_nga_c" {
   allocation_id = aws_eip.lb_ip_c.id
-  subnet_id     = aws_subnet.sdkim_pric.id
+  subnet_id     = aws_subnet.sdkim_pubc.id
   tags = {
     Name = "sdkim-nga-c"
   }
